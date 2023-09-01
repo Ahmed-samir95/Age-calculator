6192a51a021c7836e0abdb911babc5d2add229d0
ahmed-samir <ahmedsamirr126@gmail.com>
2023-09-01 16:18:40 +0200

    Add a name field to show the name next to the age in days

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 // get elementes
 let form = document.getElementsByClassName("form")
+let addname = document.getElementById("addname");
 let addage = document.getElementById("addage");
 let yourAge = document.getElementById("yourAge")
 let submitForm = document.getElementById("submit")
 
 // check input
 submitForm.onclick = function () {
-  if (addage.value === "") {
-    addage.setAttribute("placeholder", "Please enter your age in years")
+  if (addage.value === "" || addname.value === "") {
+    addage.setAttribute("placeholder", "Please enter your age in years");
+    addname.setAttribute("placeholder", "Please enter your name")
   } else{
-    yourAge.innerHTML = `your age in days is: ${calckAge()} days` 
+    yourAge.innerHTML = `Hello ${addName()} your age in days is: ${calckAge()} days` 
   }
 }
 
@@ -18,4 +20,6 @@ function calckAge() {
   return result
 }
 
-
+function addName() {
+  return addname.value
+}
