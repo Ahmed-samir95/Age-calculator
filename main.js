3b2059edca5b5f10ab55c834751152832f2cbd52
ahmed-samir <ahmedsamirr126@gmail.com>
2023-09-05 15:52:26 +0200

    Add the feature of calculating the age in hours

--- main.js
+++ main.js
@@ -2,8 +2,14 @@
 let form = document.getElementsByClassName("form")
 let addname = document.getElementById("addname");
 let addage = document.getElementById("addage");
-let yourAge = document.getElementById("yourAge")
+let yourAge = document.getElementById("yourAgeInDays")
 let submitForm = document.getElementById("submit")
+let showAgedays = document.getElementById("showagedays")
+let submitHourss = document.getElementById("submitHourss")
+let submitHours = document.getElementById("submitHours")
+let showagehours = document.getElementById("showagehours")
+let yourAgeInHours = document.getElementById("yourAgeInHours")
+console.log(yourAgeInHours);
 
 // check input
 submitForm.onclick = function () {
@@ -11,11 +17,13 @@ submitForm.onclick = function () {
     addage.setAttribute("placeholder", "Please enter your age in years");
     addname.setAttribute("placeholder", "Please enter your name")
   } else{
-    yourAge.innerHTML = `Hello ${addName()} your age in days is: ${calckAge()} days` 
+    yourAgeInDays.innerHTML = `Hello ${addName()} your age in days is: ${calckAgeInDays()} days`;
+    showAgedays.style.cssText = "display: block" 
+    submitHourss.style.cssText = "display: block"
   }
 }
 
-function calckAge() {
+function calckAgeInDays() {
   let result = addage.value * 365
   return result
 }
@@ -23,3 +31,23 @@ function calckAge() {
 function addName() {
   return addname.value
 }
+
+submitHours.onclick = function () {
+  yourAgeInHours.innerHTML = `Hello ${addName()} your age in Hours is: ${calckAgeInHours()} Hours`;
+  showagehours.style.cssText = "display: block"
+
+}
+
+function calckAgeInHours() {
+  let Hoursresult = calckAgeInDays() * 24
+  return Hoursresult
+}
+
+
+
+
+
+
+
+
+
